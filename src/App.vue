9315490f271617a5abb64310b3e3774a2e2f4a00
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-06-23 04:33:45 -0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>hay</h1>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    
+  }
+},
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  border-color: black;
+}
+</style>
