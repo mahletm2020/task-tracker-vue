129b6a805d39a775855d2396984fc4d4fe0f7205
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-06-24 23:50:40 -0700

    componenets

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+  <h1>task tracker</h1>
+  <button id="close">close</button>
+    
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 70px;
+}
+#close {
+  background-color: #b1047b;
+  border-radius: 5px;
+  color: rgb(249, 244, 200);
+  border: none;
+  height: 25px;
+  width: 100px;
+  margin-top: 15px;
+}
+</style>
---
+++ src/components/form.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="form">
+    <label for="task">Task</label>
+    <input type="text" id="task">
+    
+    <label for="datetime">Date and Time</label>
+    <input type="text" id="datetime">
+    
+    <label for="reminder">
+      <input type="checkbox" id="reminder"> Set Reminder 
+    </label>
+      <br>
+    <button class="save-button">Save Task</button>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormComponent'
+}
+</script>
+
+<style scoped>
+body {
+  background-color: black;
+  color: white;
+  font-family: Arial, sans-serif;
+  padding: 0;
+  margin: 0;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  padding: 20px;
+  background-color: #2c2c2c;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+}
+
+label {
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+input[type="text"] {
+  margin-bottom: 20px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #444;
+  color: white;
+}
+
+input[type="checkbox"] {
+  margin-right: 10px;
+  transform: scale(1.5);
+}
+
+.save-button {
+  width: 100%;
+  height: 30px;
+  background-color: #b1047b;
+  border: none;
+  border-radius: 5px;
+  color: rgb(245, 240, 196);
+  font-size: 16px;
+  cursor: pointer;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  text-align: center;
+}
+
+.save-button:hover {
+  background-color: #776274;
+}
+</style>
