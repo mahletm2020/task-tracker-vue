b6ca8adf08a0d179085e22f57f55c121e5c11650
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-06-27 08:18:56 -0700

    some change

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="container">
-     <Header tittle="task tracker"/>
-     <Form/>
+     <div class="content">
+      <Header tittle="task tracker"/>
+      <Tasks :tasks="tasks"/>
+     </div>
+     
+     <!-- <Form/> -->
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Form from './components/form.vue'
+// import Form from './components/form.vue'
+import Tasks from './components/Tasks.vue'
 export default {
 name: 'App',
 components: {
   Header,
-  Form,
+  Tasks,
+  // Form,
+},
+data(){
+  return{
+    tasks: []
+  }
+},
+created(){
+  this.tasks =[
+    {
+      id:1,
+      text: 'task 1',
+      day: 'monday',
+      reminder: true
+    },
+    {
+      id:2,
+      text: 'task 2',
+      day: 'tuesday',
+      reminder: false
+    },
+    {
+      id:3,
+      text: 'task 3',
+      day: 'wednesday',
+      reminder: true
+    }
+    
+  ]
 }
 }
 </script>
@@ -21,9 +55,14 @@ components: {
 .container {
   display: flex;
   flex-direction: column;
+  height: 26rem;
   align-items: center;
-  justify-content: center;
   border-radius: 20px;
-  border-color: black;
+  border: 1px solid rgb(239, 234, 234);
 }
+
+/* #content {
+  margin-top: 20px;
+  justify-content: top;
+} */
 </style>
